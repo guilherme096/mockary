0df277658f52ab1a2278fb4bc93271496b670f5d
guilherme096 <guilherme.rosa60@gmail.com>
2024-06-28 15:27:27 +0100

    add saving to file

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from typing import List
 import configparser
+import json
 
 
 class OpenAIRequest(BaseModel):
@@ -50,6 +51,10 @@ async def mock(data: OpenAIRequest):
 @app.post("/{mock}")
 async def preset(mock: str, samples: int):
     mock = mock.upper()
+    path = ""
+
+    if samples < 1:
+        return {"error": "Samples must be greater than 0"}
 
     request = str(mock) + "_" + str(samples)
 
@@ -67,12 +72,23 @@ async def preset(mock: str, samples: int):
             fields=mock_type["fields"].split(","),
             message=mock_type["message"],
         )
-
     except KeyError:
         return {"error": "Mock not found"}
 
-    if samples < 1:
-        return {"error": "Samples must be greater than 0"}
+    if "save" in mock_type and mock_type["save"]:
+        if "save_path" in mock_type:
+            path = mock_type["save_path"] + ".json"
+        else:
+            path = mock.lower() + ".json"
+
+        if os.path.exists(path):
+            with open(path, "r") as file:
+                data = file.read()
+
+                data = json.loads(data)
+
+                if samples <= len(data["data"]):
+                    return data["data"][1:samples]
 
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
@@ -87,5 +103,9 @@ async def preset(mock: str, samples: int):
     response = eval(completion.choices[0].message.content)
     if "cache" in mock_type and mock_type["cache"]:
         cache[request] = response
+
+    if "save" in mock_type and mock_type["save"]:
+        with open(path, "w") as file:
+            file.write(json.dumps(response))
 
     return response
